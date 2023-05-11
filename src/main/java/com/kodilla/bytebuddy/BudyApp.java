b108ef5d0fb42c6f582b2754a6ceb1523992a453
barnackles <p.martewicz@yahoo.com>
2023-05-11 16:53:22 +0200

    BuddyApp class modified. BooksGenerator and BooksFilter clasess added.

--- src/main/java/com/kodilla/bytebuddy/BudyApp.java
+++ src/main/java/com/kodilla/bytebuddy/BudyApp.java
@@ -1,26 +1,15 @@
 package com.kodilla.bytebuddy;
 
-import net.bytebuddy.ByteBuddy;
-import net.bytebuddy.dynamic.loading.ClassLoadingStrategy;
-import net.bytebuddy.implementation.FixedValue;
-import net.bytebuddy.matcher.ElementMatchers;
+import java.util.List;
 
 public class BudyApp {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
-
-        Class<?> dynamicBookClass = new ByteBuddy()
-                .subclass(Book.class)
-                .method(ElementMatchers.named("toString"))
-                .intercept(FixedValue.value(("Hello my Buddy!")))
-                .make()
-                .load(Book.class.getClassLoader(), ClassLoadingStrategy.Default.WRAPPER)
-                .getLoaded();
-
-        Class[] parameterTypes = { String.class, String.class, int.class };
-        System.out.println(dynamicBookClass.getDeclaredConstructor(parameterTypes)
-                .newInstance("title", "author", 2010));
+        List<Book> books = BooksGenerator.generate(100);
+        BooksFilter booksFilter = new BooksFilter(books);
+        List<Book> filteredBooks = booksFilter.onlyBooksOlderThan(20);
+        System.out.println(filteredBooks.size());
 
     }
 }
